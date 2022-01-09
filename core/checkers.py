71f618c5525a4fc16b161e6c1ee7d20a53975cff
kokokojo2 <foxgunplay@gmail.com>
2022-01-09 17:15:38 +0200

    AsyncChecker was implemented

--- core/checkers.py
+++ core/checkers.py
@@ -28,3 +28,31 @@ class SimpleChecker(CheckFunctionsCollectionMixin):
 
         if sleep_time:
             sleep(sleep_time)
+
+
+class AsyncChecker(CheckFunctionsCollectionMixin):
+    """
+    Runs the user-defined set of jobs (that consist of a checking function, callback and time to sleep) in an async
+    manner. Each checking function or a callback has to be async. The sleep is not blocking,
+    thus the checker will switch to another job during a sleep call.
+    """
+    def run(self):
+        asyncio.run(self._run())
+
+    async def _run(self):
+        tasks = [asyncio.create_task(self.run_job(job)) for name, job in self.job_storage.items()]
+        await asyncio.gather(*tasks)
+
+    async def run_job(self, job):
+        func = job['checker']
+        callback = job.get('callback', None)
+        sleep_time = job.get('sleep_time', None)
+
+        while True:
+            result = await func()
+
+            if callback:
+                await callback(result)
+
+            if sleep_time:
+                await asyncio.sleep(sleep_time)
